20b364ca1f0b17e680046a07ae4556e0f34b9346
Biniam batu <Biniambatu12@gmail.com>
2024-02-09 16:23:41 +0300

    initial

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+const TodoList = () => {
+  const [todos, setTodos] = useState<{ text: string; checked: boolean }[]>([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() !== "") {
+      setTodos([...todos, { text: newTodo.trim(), checked: false }]);
+      setNewTodo("");
+    }
+  };
+  const handleDeleteTodo = (index:any) => {
+      
+  };
+  const handleToggleTodo = (index:any) => {};
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={handleAddTodo} >Add</button>
+      <ul >
+        {todos.map((e, index) => (
+           <li key={index} list-style={'none'} >
+            <input 
+               type="checkbox" 
+               checked={e.checked}
+               onChange={()=>handleToggleTodo(index)}
+               />
+            <span>{e.text}</span> 
+            <button onClick={()=>handleDeleteTodo(index)}> Delete</button>
+           </li> ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
